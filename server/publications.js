586114e1009c832c4dd0c79fe23e1888f15a32b1
Charles Vestur <charles.vestur@gmail.com>
2015-10-24 10:59:03 -0700

    little clean-up

--- server/publications.js
+++ server/publications.js
@@ -1,28 +1,3 @@
 Meteor.publish('activities', function(){
 	return Activities.find({});
 });
-
-Meteor.publish('movies', function(){
-	return Movies.find({});
-});
-
-Meteor.publish('cinemas', function(){
-	return Cinemas.find({});
-});
-
-Meteor.publish('activityToModify', function(activityId){
-	return Activities.findOne({_id: activityId});
-});
-
-Meteor.publish('activitieswaiting', function(){
-	return Activitieswaiting.find({});
-});
-
-Meteor.publish('rouletteResults',function(rouletteResultsId){
-	return Activities.find({_id: {$in : rouletteResultsId}});	
-});
-
-Meteor.publish('singleActivity', function(id) {
-//  check(id, String);
-  return Activities.find(id);
-});
