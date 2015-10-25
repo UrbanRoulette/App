f5f5248140a01163125f9d778b3e9317caf7509a
léo galley <contact@kirkas.ch>
2015-10-25 11:03:09 -0700

    add card template, remove packages

---
+++ client/partials/activityItem.js
@@ -0,0 +1,31 @@
+Template.activityItem.helpers({
+  image: function() {
+    return "http://www.oubruncher.com/photos1/2099_3.jpg";
+  },
+  specific: function() {
+    return "Brunch convivial";
+  },
+  name: function() {
+    return "Le Comptoir des Arts";
+  },
+  time_start: function() {
+    return "12h15";
+  },
+  time_end: function() {
+    return "13h30";
+  },
+  address: function() {
+    return "100 rue Monge, 75005 - Métro: Censier-Daubenton";
+  },
+  description: function() {
+    return "Situé rue Monge, à la sortie du métro Censier-Daubenton, vous ne pouvez pas passer à côté du Comptoir des Arts ! Cet établissement peut se targuer de vous proposer deux ambiances pour son brunch : une plutôt calme à l'étage, où vous pourrez bruncher en toute tranquillité, l'autre un peu plus conviviale en bas. \n L'équipe du Comptoir des Arts est à vos petits soins, c'est très appréciable. Concernant le brunch, tout est fait maison, un vrai régal ! \n Commencez votre brunch par une boisson chaude (café, thé, chocolat) et une boisson fraîche (vous avez le choix parmi de nombreux jus).  \n Vous pouvez également ajouter une coupe de champagne à votre brunch (+5€).  \n Une assiette sucrée vous est servie et vous choisissez ensuite l'assiette salée de la Terre ou de la Mer.";
+  },
+  price: function() {
+    return "20$";
+  },
+  mark: function() {
+    return "5";
+  }
+});
+
+Template.activityItem.events({});
