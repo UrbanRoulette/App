7741045a869690660bbb2029f0dd97cb2ded89f1
léo galley <contact@kirkas.ch>
2015-11-15 16:29:15 -0800

    call server

--- client/partials/activityList.js
+++ client/partials/activityList.js
@@ -1,37 +1,34 @@
+var callServer = function() {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    'address': Session.get("currentSearch")
+  }, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      var center = {
+        lat: results[0].geometry.location.lat(),
+        lng: results[0].geometry.location.lng()
+      };
+      var radius = 1 / 3963.2; //Converts miles into radians
+      Meteor.apply('get_activities_results', [center, radius], true, function(error, result) {
+        if (error)
+          console.log(error);
+        else {
+          Session.set('activities_results', result);
+          console.log(result);
+        }
+      });
+    };
+  });
+};
+
+Template.activityList.onCreated(function() {
+  callServer();
+})
+
 Template.activityList.events({
   'click .activity-list__retry': function() {
-
-
-
-    var geocoder = new google.maps.Geocoder();
-
-    geocoder.geocode({
-      'address': Session.get("currentSearch")
-    }, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        var center = {
-          lat: results[0].geometry.location.lat(),
-          lng: results[0].geometry.location.lng()
-        };
-        var radius = 1 / 3963.2; //Converts miles into radians
-        Meteor.apply('get_activities_results', [center, radius], true, function(error, result) {
-          if (error)
-            console.log(error);
-          else {
-            Session.set('activities_results', result);
-            console.log(result);
-          }
-        });
-      };
-      // return error(status);
-    });
-
-
-
-
-
-  },
-
+    callServer();
+  }
 });
 
 Template.activityList.helpers({
